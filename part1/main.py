ebeb296b51feaf8479ef594d7b4c6b2cfcc5223b
stepanyanhayk <54330853+stepanyanhayk@users.noreply.github.com>
2020-10-29 21:51:42 +0400

    final commit

--- part1/main.py
+++ part1/main.py
@@ -224,8 +224,53 @@ test_cube = cubic_features(test_pca10)
 # TODO: Train your softmax regression model using (train_cube, train_y)
 #       and evaluate its accuracy on (test_cube, test_y).
 
-theta, cost_function_history = softmax_regression(train_cube, train_y, temp_parameter=1, alpha=0.3, lambda_factor=1.0e-4, k=10, num_iterations=150)
-plot_cost_function_over_time(cost_function_history)
-test_error = compute_test_error(test_cube, test_y, theta, temp_parameter=1)
-print("test error for pca10: ", test_error)
+# theta, cost_function_history = softmax_regression(train_cube, train_y, temp_parameter=1, alpha=0.3, lambda_factor=1.0e-4, k=10, num_iterations=150)
+# plot_cost_function_over_time(cost_function_history)
+# test_error = compute_test_error(test_cube, test_y, theta, temp_parameter=1)
+# print("test error for pca10: ", test_error)
 
+
+n = 5000
+n_test = 2000
+k = 10  # number of categories
+indices_train = np.random.permutation(n)
+indices_test = np.random.permutation(n_test)
+
+train_x_trunc = train_x[indices_train,:]
+train_y_trunc = train_y[indices_train]
+test_x_trunc = test_x[indices_test,:]
+test_y_trunc = test_y[indices_test]
+
+# Find PCA representation of training and test sets
+n_components = 18
+pcs = principal_components(train_x_trunc)
+train_pca = project_onto_PC(train_x_trunc, pcs, n_components, feature_means)
+test_pca = project_onto_PC(test_x_trunc, pcs, n_components, feature_means)
+
+# Kernel matrix for training data
+kernel_train = rbf_kernel(train_pca, train_pca, gamma=0.5)
+
+# Kernel matrix for test data
+kernel_test = rbf_kernel(train_pca, test_pca, gamma=0.5)
+    
+def run_kernel_softmax_on_MNIST(kernel_train, train_y, kernel_test, test_y, \
+                                temp_parameter=1.0, lambda_factor=0.01, \
+                                k=10, learning_rate=0.3, num_iterations=150):
+
+    # Softmax kernel regression
+    alphas, cost_function_history = softmax_kernel_regression(train_y, kernel_train, \
+                                    temp_parameter, learning_rate, \
+                                    lambda_factor, k, num_iterations)
+    # Plot cost vs iteration
+    plot_cost_function_over_time(cost_function_history)
+    
+    # Calculate test error
+    test_error_kernel = compute_kernel_test_error(alphas, kernel_test, test_y, temp_parameter=1)
+    
+    return (test_error_kernel, alphas)
+
+test_error_kernel, alphas = run_kernel_softmax_on_MNIST(kernel_train, train_y_trunc, \
+                    kernel_test, test_y_trunc, temp_parameter=0.5, \
+                    lambda_factor=0.01, k=10, learning_rate=0.3, num_iterations=150)
+
+print("Test error for kernelized softmax regression:", test_error_kernel) 
--- part1/softmax.py
+++ part1/softmax.py
@@ -177,3 +177,162 @@ def compute_test_error(X, Y, theta, temp_parameter):
     error_count = 0.
     assigned_labels = get_classification(X, theta, temp_parameter)
     return 1 - np.mean(assigned_labels == Y)
+
+def compute_probabilities_kernel(alpha_matrix, kernel_matrix, temp_parameter):
+    factor = alpha_matrix @ kernel_matrix / temp_parameter
+    c = np.max(factor, axis=0)
+    H = np.exp(factor - c) / np.exm(factor - c).sum()
+    return H
+
+def compute_kernel_cost_function(alpha_matrix, kernel_matrix, Y, lambda_factor, temp_parameter):
+    """
+    Computes the total cost over every datapoint.
+    Args:
+        alpha_matrix - (k, n) NumPy array where row j represents alpha values for
+                label j
+        kernel_matrix - (n, n) NumPy array (similarity matrix, each column: phi(x_1).phi(x_1)
+        to phi(x_n).phi(x_1))
+        Y - (n, ) NumPy array containing the labels (a number from 0-9) for each
+            data point
+        lambda_factor - the regularization constant (scalar)
+        temp_parameter - the temperature parameter of softmax function (scalar)
+    Returns
+        c - the cost value (scalar)
+    """
+    
+    # Get number of categories
+    k = alpha_matrix.shape[0]
+    
+    # Get number of examples
+    n = kernel_matrix.shape[0]
+    
+    ### avg error term ###
+    
+    # Clip prob matrix to avoid NaN instances
+    clip_prob_matrix = np.clip(compute_kernel_probabilities(alpha_matrix, \
+                            kernel_matrix, temp_parameter), 1e-15, 1-1e-15)
+    
+    # Take the log of the matrix of probabilities
+    log_clip_matrix = np.log(clip_prob_matrix)
+    
+    # Create a sparse matrix of [[y(i) == j]]
+    M = sparse.coo_matrix(([1]*n, (Y, range(n))), shape = (k,n)).toarray()
+    
+    # Only add terms of log(matrix of prob) where M == 1
+    error_term = (-1/n)*np.sum(log_clip_matrix[M == 1])    
+                
+    ### Regularization term ###
+    reg_term = (lambda_factor/2)*np.linalg.norm(alpha_matrix)**2
+    
+    return error_term + reg_term
+
+def compute_kernel_gradient(alpha_matrix, kernel_matrix, Y, lambda_factor, temp_parameter):
+    """ 
+    Computes the gradient of the cost function with respect to alphas
+    Args:
+        alpha_matrix - (k, n) NumPy array where row j represents alpha values for
+                label j
+        kernel_matrix - (n, n) NumPy array (similarity matrix, each column: phi(x_1).phi(x_1)
+        to phi(x_n).phi(x_1))
+        Y - (n, ) NumPy array containing the labels (a number from 0-9) for each
+            data point
+        learning_rate - the learning rate, alpha or eta (scalar)
+        lambda_factor - the regularization constant (scalar)
+        temp_parameter - the temperature parameter of softmax function (scalar)
+    """
+    # Get number of labels
+    k = alpha_matrix.shape[0]
+    
+    # Get number of examples
+    n = kernel_matrix.shape[0]
+    
+    # Create spare matrix of [[y(i) == j]]
+    M = sparse.coo_matrix(([1]*n, (Y, range(n))), shape=(k,n)).toarray()
+    
+    # Matrix of Probabilities
+    P = compute_kernel_probabilities(alpha_matrix, kernel_matrix, temp_parameter)
+    
+    # Gradient matrix of theta
+    grad_alpha = (-1/(temp_parameter*n))*((M - P) @ kernel_matrix) + lambda_factor*alpha_matrix
+    
+    return grad_alpha
+
+def run_kernel_gradient_descent_iteration(alpha_matrix, kernel_matrix, Y, \
+                            learning_rate, lambda_factor, temp_parameter):
+    """
+    Runs one step of batch gradient descent
+    Args:
+        alpha_matrix - (k, n) NumPy array where row j represents alpha values for
+                label j
+        kernel_matrix - (n, n) NumPy array (similarity matrix, each column: phi(x_1).phi(x_1)
+        to phi(x_n).phi(x_1))
+        Y - (n, ) NumPy array containing the labels (a number from 0-9) for each
+            data point
+        learning_rate - the learning rate, alpha or eta (scalar)
+        lambda_factor - the regularization constant (scalar)
+        temp_parameter - the temperature parameter of softmax function (scalar)
+    Returns:
+        alpha - (k, n) NumPy array that is the final value of alpha
+    """
+    
+    # Gradient matrix of theta
+    grad_alpha = compute_kernel_gradient(alpha_matrix, kernel_matrix, Y, \
+                                         lambda_factor, temp_parameter)
+    
+    # Gradient descent update of theta matrix
+    alpha_matrix = alpha_matrix - learning_rate*grad_alpha
+    
+    return alpha_matrix
+
+def softmax_kernel_regression(Y, kernel_matrix, temp_parameter, learning_rate, \
+                              lambda_factor, k, num_iterations):
+    """
+    Runs batch gradient descent for a specified number of iterations on a dataset
+    with alphas initialized to the all-zeros array. Here, alpha is a k by n NumPy array
+    where row j represents the alpha values of our model for label j for
+    j = 0, 1, ..., k-1
+    Args:
+        Y - (n, ) NumPy array containing the labels (a number from 0-9) for each
+            data point
+        kernel_matrix - (n, n) NumPy array (similarity matrix, each column: phi(x_1).phi(x_1)
+        to phi(x_n).phi(x_1))
+        temp_parameter - the temperature parameter of softmax function (scalar)
+        learning_rate - the learning rate, alpha or eta (scalar)
+        lambda_factor - the regularization constant (scalar)
+        k - the number of labels (scalar)
+        num_iterations - the number of iterations to run gradient descent (scalar)
+    Returns:
+        alpha - (k, n) NumPy array that is the final value of alpha
+        cost_function_progression - a Python list containing the cost calculated at each step of gradient descent
+    """
+ 
+    alphas = np.zeros([k, len(Y)])
+    cost_function_progression = []
+    for i in range(num_iterations):
+        cost_function_progression.append(compute_kernel_cost_function(alphas, kernel_matrix, \
+                                    Y, lambda_factor, temp_parameter))
+        alphas = run_kernel_gradient_descent_iteration(alphas, kernel_matrix, Y, learning_rate, \
+                                                lambda_factor, temp_parameter)
+    return alphas, cost_function_progression
+
+def get_kernel_classification(alpha_matrix, kernel_matrix, temp_parameter):
+    """
+    Makes predictions by classifying a given dataset
+    Args:
+        alpha_matrix - (k, n) NumPy array where row j represents alpha values for label j
+        kernel_matrix - (n, n) NumPy array (similarity matrix, each column: phi(x_1).phi(x_1)
+        to phi(x_n).phi(x_1)). For the test set, kernel_matrix is (n,m) where m
+        is the number of examples in the test set.
+        temp_parameter - the temperature parameter of softmax function (scalar)
+    Returns:
+        Predicted Y - (n, ) NumPy array, containing the predicted label (a number between 0-9) for
+        each data point. For the test set, predicted Y is (m, ), where m is the
+        number of examples in the test set.
+    """
+    
+    probabilities = compute_kernel_probabilities(alpha_matrix, kernel_matrix, temp_parameter)
+    return np.argmax(probabilities, axis = 0)
+
+def compute_kernel_test_error(alpha_matrix, kernel_matrix, Y, temp_parameter):
+    assigned_labels = get_kernel_classification(alpha_matrix, kernel_matrix, temp_parameter)
+    return 1 - np.mean(assigned_labels == Y)
